374cf23536e58f5c99a0e1bdb097b997f1babf93
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2023-10-02 12:38:13 +0200

    ajouts login (A retirer)

--- my-library/client/src/table/table.js
+++ my-library/client/src/table/table.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 function table() {
-    return (
-        <>
-            <div className='postProfile'>
-                {/* <img>cover</img> */}
-                <div className='postElement1'><p>titre</p></div>
-                <div className='postElement2'>auteur</div>
-            </div >
-        </>
-    )
+  return (
+    <>
+      <div className="postProfile">
+        {/* <img>cover</img> */}
+        <div className="postElement1">
+          <p>titre</p>
+        </div>
+        <div className="postElement2">auteur</div>
+      </div>
+    </>
+  );
 }
 
-export default table
+export default table;
